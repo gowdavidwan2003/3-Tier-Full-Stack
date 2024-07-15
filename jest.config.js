c85af27ebf0e5aff965f75e120cb564202c94a53
gowdavidwan2003 <107876507+gowdavidwan2003@users.noreply.github.com>
2024-07-15 23:54:06 +0530

    included Test

---
+++ jest.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    testEnvironment: 'node',
+    coverageDirectory: 'coverage',
+    collectCoverage: true,
+    coverageReporters: ['text', 'lcov'],
+  };
+  
---
+++ tests/campgrounds.test.js
@@ -0,0 +1,84 @@
+describe('Simulated test suite', () => {
+  it('should pass unit test 1', (done) => {
+    setTimeout(() => {
+      expect(true).toBe(true);
+      done();
+    }, 1000); // 100 milliseconds delay
+  });
+
+  it('should pass unit test 2', (done) => {
+    setTimeout(() => {
+      expect(true).toBe(true);
+      done();
+    }, 1500); // 150 milliseconds delay
+  });
+
+  it('should pass integration test', (done) => {
+    setTimeout(() => {
+      expect(true).toBe(true);
+      done();
+    }, 2000); // 2 seconds delay
+  });
+
+  it('should pass integration test', (done) => {
+    setTimeout(() => {
+      expect(true).toBe(true);
+      done();
+    }, 3000); // 3 seconds delay
+  });
+
+  it('should pass integration test', (done) => {
+    setTimeout(() => {
+      expect(true).toBe(true);
+      done();
+    }, 2000); // 2 seconds delay
+  });
+});
+
+describe('Simulated test suite-1', () => {
+  it('should pass unit test 1', (done) => {
+    setTimeout(() => {
+      expect(true).toBe(true);
+      done();
+    }, 1000); // 100 milliseconds delay
+  });
+
+  it('should pass unit test 2', (done) => {
+    setTimeout(() => {
+      expect(true).toBe(true);
+      done();
+    }, 1500); // 150 milliseconds delay
+  });
+
+
+});
+
+describe('Simulated test suite-2', () => {
+  it('should pass unit test 1', (done) => {
+    setTimeout(() => {
+      expect(true).toBe(true);
+      done();
+    }, 1000); // 100 milliseconds delay
+  });
+
+  it('should pass unit test 2', (done) => {
+    setTimeout(() => {
+      expect(true).toBe(true);
+      done();
+    }, 1500); // 150 milliseconds delay
+  });
+
+  it('should pass integration test', (done) => {
+    setTimeout(() => {
+      expect(true).toBe(true);
+      done();
+    }, 2000); // 2 seconds delay
+  });
+
+  it('should pass integration test', (done) => {
+    setTimeout(() => {
+      expect(true).toBe(true);
+      done();
+    }, 3000); // 3 seconds delay
+  });
+});
